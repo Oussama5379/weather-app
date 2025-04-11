7f5b121072cfb120e4eeb1473348e9934b7c7a7f
Oussama Lejmi <oussama.lejmi@insat.ucar.tn>
2025-04-11 19:22:04 +0100

    create the containers that will contain the weather info

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
 		filename: "main.js",
 		path: path.resolve(__dirname, "dist"),
 		clean: true,
+		assetModuleFilename: "assets/videos/[name][ext][query]",
 	},
 	devtool: "eval-source-map",
 	devServer: {
@@ -23,6 +24,10 @@ module.exports = {
 			{
 				test: /\.css$/i,
 				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.(mp4|webm|ogg)$/i,
+				type: "asset/resource",
 			},
 		],
 	},
